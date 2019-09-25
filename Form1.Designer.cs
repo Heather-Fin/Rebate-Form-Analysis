422839f6a9f901f6f7cc27e7c753329c2ed83a31
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-09-25 13:45:57 -0500

    Opens file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_record_num = new System.Windows.Forms.Label();
+            this.lbl_record_num_data = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_record_num
+            // 
+            this.lbl_record_num.AutoSize = true;
+            this.lbl_record_num.Location = new System.Drawing.Point(38, 91);
+            this.lbl_record_num.Name = "lbl_record_num";
+            this.lbl_record_num.Size = new System.Drawing.Size(102, 13);
+            this.lbl_record_num.TabIndex = 0;
+            this.lbl_record_num.Text = "Number of Records:";
+            // 
+            // lbl_record_num_data
+            // 
+            this.lbl_record_num_data.AutoSize = true;
+            this.lbl_record_num_data.Location = new System.Drawing.Point(147, 91);
+            this.lbl_record_num_data.Name = "lbl_record_num_data";
+            this.lbl_record_num_data.Size = new System.Drawing.Size(0, 13);
+            this.lbl_record_num_data.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(443, 456);
+            this.Controls.Add(this.lbl_record_num_data);
+            this.Controls.Add(this.lbl_record_num);
+            this.Name = "Form1";
+            this.Text = "Rebate Form Analysis";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_record_num;
+        private System.Windows.Forms.Label lbl_record_num_data;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
+// Author: Heather Finnegan
+// Class: CS 6326 - Human Computer Interaction
+// Date: 09/25/2019
+// Description: Analyzes a text file produced by assignment 2 to allow users to evaluate
+// the effectiveness of the user interface for the rebate form.
 
 namespace Asg3_HXF180007
 {
@@ -15,6 +22,38 @@ namespace Asg3_HXF180007
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InputFile.loadFile();
+        }
+    }
+
+    // Opens the user defined input file and evaluates the data.
+    public class InputFile{
+
+        // Handles opening and reading the user defined input file.
+        public static void loadFile()
+        {
+            string fileName = "CS6326Asg2.txt";
+
+            if (File.Exists(fileName))
+            {
+                StreamReader sr = new StreamReader(fileName);
+
+                while (sr.Peek() > -1)
+                {
+                    string line = sr.ReadLine();
+                    string[] element = line.Split('\t');
+
+                    for (int i = 0; i < 12; i++)
+                    {
+                        Console.WriteLine(element[i]);
+                    }
+                }
+                sr.Close();
+            }
         }
     }
 }
